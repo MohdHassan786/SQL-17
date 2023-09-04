9ae67631cb45ae4aa2a13e5e8b747ab60362d8ed
mohdhassan7890@gmail.com <mohdhassan7890@gmail.com>
2023-09-04 16:22:56 +0530

    first commit

---
+++ SQL-17/Data/MovieDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SQL_17.Model;
+
+namespace SQL_17.Data
+{
+    public class MovieDbContext : DbContext
+    {
+        public MovieDbContext (DbContextOptions<MovieDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SQL_17.Model.Movie> Movie { get; set; } = default!;
+    }
+}
